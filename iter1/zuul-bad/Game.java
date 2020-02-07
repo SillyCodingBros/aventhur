9c70380e1870cdc8d6e8531e9e2d22794aa28c13
BruhP8 <luc.biohazard@gmail.com>
2020-02-08 00:16:43 +0100

    make it better - show commands

--- iter1/zuul-bad/Game.java
+++ iter1/zuul-bad/Game.java
@@ -185,11 +185,8 @@ public class Game
      */
     private void printHelp()
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
-        System.out.println();
-        System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("Your commands are : ");
+        parser.showCommands();
     }
 
     /**
