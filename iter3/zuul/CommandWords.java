e0e7c0597d03aa9c16f002b775294f94a841150e
TheTitoPlay <vocatis.stark@gmail.com>
2020-03-18 21:37:58 +0100

    Init Iter 3

---
+++ iter3/zuul/CommandWords.java
@@ -0,0 +1,66 @@
+/**
+ * This class is part of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.
+ *
+ * This class holds an enumeration of all command words known to the game.
+ * It is used to recognise commands as they are typed in.
+ *
+ * @author  Michael Kolling and David J. Barnes
+ * @version 2006.03.30
+ */
+
+public class CommandWords
+{
+    // a constant array that holds all valid command words
+    private static final String[] validCommands = {
+        "go", "quit", "help", "look", "eat", "back"
+    };
+
+    /**
+     * Constructor - initialise the command words.
+     */
+    public CommandWords()
+    {
+        // nothing to do at the moment...
+    }
+
+    /**
+     * Check whether a given String is a valid command word.
+     * @return true if a given string is a valid command,
+     * false if it isn't.
+     */
+    public boolean isCommand(String aString)
+    {
+        for(int i = 0; i < validCommands.length; i++) {
+            if(validCommands[i].equals(aString))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
+    }
+
+    /**
+     * Return all valid commands as a String.
+     */
+    public String getCommandList()
+    {
+      /*
+      StringBuilder sb = new StringBuilder();
+      for (String s : hugeArray) {
+          sb.append(s);
+      }
+      String result = sb.toString();
+      */
+      StringBuilder sb = new StringBuilder("");
+      //String commandList = "";
+        for(String command : validCommands) {
+          //commandList += (command + " ");
+          sb.append(command);
+          sb.append(" ");
+          //System.out.print(command + " ");
+        }
+        //return commandList;
+        return sb.toString();
+        //System.out.println();
+    }
+}
---
+++ iter3/zuul/Item.java
@@ -0,0 +1,99 @@
+/**
+ * Class Item - an item in the game
+ *
+ * An "Item" represents one object of the game,
+ * it could have a description, a long description,
+ * a weight, a price. Value set -1 if not used.
+ *
+ **/
+
+public class Item
+{
+
+  private String description;
+  private String longDescription;
+  private String comment;
+  private Integer price = -1;
+  private Integer weight = -1;
+
+  /**
+  * Return the description of the item.
+  * @return The description of the item.
+  */
+  public String getDescription(){
+    return description;
+  }
+
+  /**
+  * Return the long description of the item.
+  * @return The long description of the item.
+  */
+  public String getLongDescription(){
+    return description + longDescription;
+  }
+
+  /**
+  * Return the finding comment of the item.
+  * @return The comment of the item.
+  */
+  public String getComment(){
+    return comment;
+  }
+
+  /**
+  * Return the price of the item.
+  * @return The price of the item.
+  */
+  public Integer getPrice(){
+    return price;
+  }
+
+  /**
+  * Return the weight of the item.
+  * @return The weight of the item.
+  */
+  public Integer getWeight(){
+    return weight;
+  }
+
+  /**
+   * Set the description of the given item.
+   * @param description The description for the item.
+   */
+  public void setDescription(String description){
+    this.description = description;
+  }
+
+  /**
+   * Set the long description of the given item.
+   * @param description The long description for the item.
+   */
+  public void setLongDescription(String description){
+    this.longDescription = description;
+  }
+
+  /**
+   * Set the finding comment of the given item.
+   * @param comment The comment for the item.
+   */
+  public void setCommment(String comment){
+    this.comment = comment;
+  }
+
+  /**
+   * Set the price description of the given item.
+   * @param price The price for the item.
+   */
+  public void setPrice(Integer price){
+    this.price = price;
+  }
+
+  /**
+   * Set the weight of the given item.
+   * @param weight The weight for the item.
+   */
+  public void setWeight(Integer weight){
+    this.weight = weight;
+  }
+
+}
