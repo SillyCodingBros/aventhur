8cd90ed1e9c236c9bd36cdfe96fa9435573a6efe
BruhP8 <luc.biohazard@gmail.com>
2020-04-17 00:46:04 +0200

    door cleanup

--- iter4/zuul_iter4/Door.java
+++ iter4/zuul_iter4/Door.java
@@ -9,24 +9,15 @@
     private Boolean isLocked;
     private Item unlockItem;
     private String description;
-    //private Boolean isTrap;
-    //private Room r1;
-    //private Room r2;
-    //private Boolean direction; // in case door is a trap : 0 for r1->r2, 1 for r2->r1
 
     /**
      * Create a door to door block rooms
      * @param key The room's description.
      */
     public Door(Item key, String description /*, Boolean trap, Room r1, Room r2, Boolean direction*/){
-        //this.isLocked = isLocked;
         this.isLocked = true;
         this.unlockItem = key;
         this.description = description;
-        //this.isTrap = trap;
-        //this.r1 = r1;
-        //this.r2 = r1;
-        //this.direction = direction;
     }
 
     /**
@@ -49,34 +40,4 @@
     {
         return description;
     }
-
-
-
-
-    /*
-    public Boolean getLocked(){
-        return this.isLocked;
-    }
-
-    public Item getKey(){
-        return this.unlockItem;
-    }
-
-
-    public Boolean isTrap(){
-        return this.isTrap;
-    }
-
-    public Boolean getDoorDirection(){
-        return this.direction;
-    }
-
-    public void setLoked(Boolean newState){
-        this.isLocked = newState;
-    }
-
-    public void setTrap(Boolean newState){
-        this.isTrap = newState;
-    }
-    */
  }
